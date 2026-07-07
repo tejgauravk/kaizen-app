b5b8084c686a7b8ccfb0ebac74f1706e053c9bbf
tejgauravk <tejgauravk@gmail.com>
2026-07-07 13:41:32 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -15,11 +15,12 @@
    file is added), so the old cache gets cleaned up.
    ========================================================================= */
 
-const CACHE_VERSION = "kaizen-v4";
+const CACHE_VERSION = "kaizen-v5";
 
 const APP_SHELL = [
   "./",
   "./index.html",
+  "./login.html",
   "./style.css",
   "./app.js",
   "./storage.js",
